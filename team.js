a62615948309197a13e37957b361f1789ab6b336
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2024-05-28 19:03:12 +0530

    first commit

--- team.js
+++ team.js
@@ -1,15 +1,7 @@
-let team100 = [
-  "A SHARMA",
-  "H KLAASEN",
-  "Y JAISWAL",
-  "T HEAD",
-  "P CUMMINIS",
-  "S SAMASAN",
-  "R PARAG",
-];
-let team75 = ["T BOULT", "T NATARAJAN", "Y CHAHAL", "R ASHWIN", "K N REDDY"];
-let team50 = ["A KHAN", " R TRIPATHI", "A MARKRAM", "B KUMAR", "T CADMORE"];
-let team25 = ["S SHARMA", "R POWELL", "D JUREL", "J UNADKAT"];
+let team100 = [95, 80, 66, 47, 35, 21, 8];
+let team75 = [79, 35, 32, 8, 5];
+let team50 = [99, 68, 13, 2, 21];
+let team25 = [64, 6, 91, 10];
 
 var total = [];
 var duplicate = [];
@@ -55,18 +47,51 @@ function shuffle(arr, arr1, arr2, arr3) {
     ];
   }
   temp1 = [
-    
-      ...arrTemp100.slice(0, 4),
-      ...arrTemp75.slice(0, 3),
-      ...arrTemp50.slice(0, 2),
-      ...arrTemp25.slice(0, 2),
-    
+    ...arrTemp100.slice(0, 4),
+    ...arrTemp75.slice(0, 3),
+    ...arrTemp50.slice(0, 2),
+    ...arrTemp25.slice(0, 2),
   ];
   temp1.sort((a, b) => a - b);
-  total.push(temp1);
+  total.push([
+    ...arrTemp100.slice(0, 4),
+    ...arrTemp75.slice(0, 3),
+    ...arrTemp50.slice(0, 2),
+    ...arrTemp25.slice(0, 2),
+  ]);
 }
-for (i = 0; i < 16; i++) {
-  shuffle(team100, team75, team50, team25);
+function gen() {
+  var num =(document.getElementById("count")).value
+  console.log(num)
+  for (i = 0; i < num; i++) {
+    shuffle(team100, team75, team50, team25);
+  }
+  console.log(check_duplicate_in_array(duplicate));
+  console.log(total);
+  var list = document.getElementById("id01");
+  var table = document.createElement("table");
+  for (i = 0; i < total.length; i++) {
+    var tr = document.createElement("tr");
+    td = document.createElement("td");
+    td.innerText = i + 1;
+    tr.append(td);
+    temp = total[i];
+    store = 0;
+    for (j = 0; j < temp.length; j++) {
+      td = document.createElement("td");
+      var store = store + temp[j];
+      td.innerText = temp[j];
+      tr.append(td);
+      if (j == 10) {
+        td = document.createElement("li");
+        td.innerText = store;
+        list.append(td);
+      }
+      table.append(tr);
+    }
+  }
+  cont = document.getElementById("container");
+  cont.append(table);
 }
 
 const check_duplicate_in_array = (input_array) => {
@@ -76,23 +101,24 @@ const check_duplicate_in_array = (input_array) => {
   return Array.from(new Set(duplicates));
 };
 
-console.log(check_duplicate_in_array(duplicate));
-console.log(total);
+function sortList() {
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("id01");
+  switching = true;
+  while (switching) {
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    for (i = 0; i < b.length - 1; i++) {
+      shouldSwitch = false;
 
-
-var table=document.createElement("table")
-for(i=0;i<total.length;i++){
-  var tr=document.createElement("tr")
-  td=document.createElement("td")
-  td.innerText=i+1
-  tr.append(td)
-  temp=total[i]
-  for(j=0;j<temp.length;j++){
-    td=document.createElement("td")
-    td.innerText=temp[j]
-    tr.append(td)
+      if (Number(b[i].innerHTML) < Number(b[i + 1].innerHTML)) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
   }
-  table.append(tr)
 }
-cont=document.getElementById("container")
-cont.append(table)
